b599c51629def4e8deef8b2398e62058921a8998
breakingBrad <bradvanorman@gmail.com>
2018-05-16 20:12:48 -0600

    completed through step 9, black diamond not yet attempted

--- app/src/components/Calculator/Calculator.js
+++ app/src/components/Calculator/Calculator.js
@@ -57,6 +57,16 @@ class Calculator extends Component {
     this.setState( { display: String(result) } );
   }
 
+  clearDisplay() {
+    this.setState( {
+      header: 'Calculator',
+      display: '0',
+      operator: '',
+      temp: 0,
+      resetDisplay: false,
+    } );
+  }
+
   render() {
     return (
       <div id="calculator-container">
@@ -69,18 +79,18 @@ class Calculator extends Component {
             <span className="total"> { this.state.display } </span>
           </div>
     
-          <div className="btn clear"></div>
+          <div className="btn clear"      onClick = { () => { this.clearDisplay() } } ></div>
     
-          <div className="btn zero"   onClick = { () => { this.setDisplay('0'); } }></div>
-          <div className="btn one"    onClick = { () => { this.setDisplay('1'); } }></div>
-          <div className="btn two"    onClick = { () => { this.setDisplay('2'); } }></div>
-          <div className="btn three"  onClick = { () => { this.setDisplay('3'); } }></div>
-          <div className="btn four"   onClick = { () => { this.setDisplay('4'); } }></div>
-          <div className="btn five"   onClick = { () => { this.setDisplay('5'); } }></div>
-          <div className="btn six"    onClick = { () => { this.setDisplay('6'); } }></div>
-          <div className="btn seven"  onClick = { () => { this.setDisplay('7'); } }></div>
-          <div className="btn eight"  onClick = { () => { this.setDisplay('8'); } }></div>
-          <div className="btn nine"   onClick = { () => { this.setDisplay('9'); } }></div>
+          <div className="btn zero"       onClick = { () => { this.setDisplay('0'); } }></div>
+          <div className="btn one"        onClick = { () => { this.setDisplay('1'); } }></div>
+          <div className="btn two"        onClick = { () => { this.setDisplay('2'); } }></div>
+          <div className="btn three"      onClick = { () => { this.setDisplay('3'); } }></div>
+          <div className="btn four"       onClick = { () => { this.setDisplay('4'); } }></div>
+          <div className="btn five"       onClick = { () => { this.setDisplay('5'); } }></div>
+          <div className="btn six"        onClick = { () => { this.setDisplay('6'); } }></div>
+          <div className="btn seven"      onClick = { () => { this.setDisplay('7'); } }></div>
+          <div className="btn eight"      onClick = { () => { this.setDisplay('8'); } }></div>
+          <div className="btn nine"       onClick = { () => { this.setDisplay('9'); } }></div>
     
           <div className="btn equal"      onClick = { () => { this.calculate(); } }></div>
           <div className="btn multiply"   onClick = { () => { this.setOperator('*'); } }></div>
